c956c89da786b367b1e4863cc4a460ffc02460fb
viserys0219 <viserys936@gmail.com>
2015-03-17 16:57:49 +0800

    add slug filed to Category

--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+        print super(Category)
+        print super(Category, self)
+        print super()
 
     def __unicode__(self):
         return self.name
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from rango.models import Category
 # from django.http import HttpResponse
 
 def index(request):
-    context_text = {"boldmessage": "I am bold font from the context"}
-    return render(request, 'rango/index.html', context_text)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context_text = {"about_message": "Rango says here is the about page"}
